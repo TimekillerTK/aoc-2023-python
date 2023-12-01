fe6d8611ef57fe003b5a27ceebfe536a67aa3bd8
TimekillerTK <erwartungen@protonmail.com>
2023-12-01 16:05:42 +0100

    day01

--- aoc.py
+++ aoc.py
@@ -1,9 +1,25 @@
-from aoc_2023_python.aoc_days.day01 import name_of_day01_func
+from aoc_2023_python.aoc_days.day01 import d01_trebuchet_one, d01_trebuchet_two
+from aoc_2023_python.lib.helpers import input_loader
 
 
 def aoc():
-    name_of_day01_func()
-    print("Hello World!")
+    calibration_document_example1 = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+    calibration_document_example2 = [
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen",
+    ]
+    calibration_document_list = input_loader("day01")
+
+    d01_trebuchet_one(calibration_document=calibration_document_example1)
+    d01_trebuchet_one(calibration_document=calibration_document_list)
+
+    d01_trebuchet_two(calibration_document_example2)
+    d01_trebuchet_two(calibration_document=calibration_document_list)
 
 
 if __name__ == "__main__":
--- aoc_2023_python/aoc_days/day01.py
+++ aoc_2023_python/aoc_days/day01.py
@@ -1,4 +1,94 @@
+def find_strdigits_in_line(line: str) -> list[tuple[int, int]]:
+    digits: list[tuple[int, int]] = []
+
+    number_mapping = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+
+    for word in number_mapping.keys():
+        start_index = 0
+        while start_index is not -1:
+            start_index = line.find(word, start_index)
+            if start_index is not -1:
+                digits.append((start_index, number_mapping[word]))
+                start_index += len(word)
+
+    print(f"StrDigits Found: {digits}")
+    return digits
 
 
-def name_of_day01_func():
-    print("Exercise for Day 01!")
+def find_intdigits_in_line(line: str) -> list[tuple[int, int]]:
+    digits: list[tuple[int, int]] = []
+
+    print(f"")
+    for index, character in enumerate(line):
+        if character.isdigit():
+            print(f"INDEX: {index}, CHAR: {character}")
+            digits.append((index, int(character)))
+
+    print(f"IntDigits Found: {digits}")
+    return digits
+
+
+def combine_first_and_last_digits(digits: list[int]) -> int:
+    first_digit: int = digits[0]
+    last_digit: int = digits[-1]
+
+    return_value = first_digit * 10 + last_digit
+    print(f"Converted {digits} -> {return_value}")
+    return return_value
+
+
+def d01_trebuchet_one(calibration_document: list[str]) -> int:
+    print("Exercise for Day 01 - Part 1!")
+    print("-----------------------------")
+    print()
+
+    list_of_numbers: list[int] = []
+
+    for line in calibration_document:
+        digits = find_intdigits_in_line(line=line)
+        number = combine_first_and_last_digits(digits=[x[1] for x in digits])
+        print(f"({line} has number {number})")
+        print()
+        list_of_numbers.append(number)
+
+    return_value = sum(list_of_numbers)
+    print(f" ---------------------------------------------")
+    print(f" --- Sum of all numbers is {return_value} --- ")
+    print(f" ---------------------------------------------")
+
+    return return_value
+
+
+def d01_trebuchet_two(calibration_document: list[str]) -> int:
+    print("Exercise for Day 01 - Part 2!")
+    print("-----------------------------")
+    print()
+
+    list_of_numbers: list[int] = []
+
+    for line in calibration_document:
+        print("=====================")
+        print(f"Current line: {line}")
+        digits = find_intdigits_in_line(line=line)
+        words = find_strdigits_in_line(line=line)
+
+        sorted_line = sorted(digits + words, key=lambda x: x[0])
+        number = combine_first_and_last_digits(digits=[x[1] for x in sorted_line])
+        list_of_numbers.append(number)
+
+    return_value = sum(list_of_numbers)
+    print(f" ---------------------------------------------")
+    print(f" --- Sum of all numbers is {return_value} --- ")
+    print(f" ---------------------------------------------")
+
+    return return_value
